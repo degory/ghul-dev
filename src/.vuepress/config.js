a609e5ae6bb40e711700c78dfcf2735e92fc1429
degory <degs@giantblob.com>
2024-01-19 12:30:16 +0000

    Factor out duplicate PrismJS ghul initialization

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,38 +1,8 @@
-const { description } = require('../../package')
+const { description } = require('../../package');
 
-const Prism = require('prismjs');
+const { getPrismConfiguredForGhul } = require('./getPrismConfiguredForGhul');
 
-if (!Prism.languages.ghul) {
-
-  console.log('set-up Prism.languages.ghul...');
-
-  Prism.languages.ghul = {
-    'comment': {
-      pattern: /(^|[^\\])(?:\/\*[\s\S]*?\*\/|(?:\/\/).*)/,
-      lookbehind: true
-    },
-    'string' : {
-      pattern: /(^|[^@\\])("|')(?:\\?[\s\S])*?\2/,
-      greedy: true,
-      lookbehind: true
-    },
-    'control': /\b(assert|if|then|elif|else|fi|for|in|while|do|od|continue|break|case|when|default|esac|throw|try|catch|finally|yrt|return)\b/,
-    'keyword': /\b(new|cast|isa|namespace|class|trait|enum|use|is|si|let|ptr|ref|static|public|protected|private|typeof|->|=>)\b/,
-    'boolean': /\b(?:true|false|null)\b/,
-    'class-name': /\b([A-Z][A-Za-z0-9_`]*|void|bool|byte|ubyte|short|ushort|int|uint|long|ulong|word|uword|char|object|string)\b/,
-    'function': /\b([a-zA-Z_][a-zA-Z0-9_]*)(?=\s*\()/, 
-    'number': /\b-?\d*\.?[\d]+\b/,
-    'operator': /(!|\$|%|\^|&|\*|-|\+|=|\||@|~|#|\\|<|>|\.|\?|\/|\*)+/,
-    'punctuation': /[;[\](),.]/,
-    'variable': /\b([`a-zA-Z_][a-zA-Z0-9_]*)\b/,		
-  }
-
-  console.log('set-up Prism.languages.ghul...done');
-} else {
-  console.log('Prism.languages.ghul already set-up');
-}
-
-console.log('languages is now: ', Prism.languages);
+getPrismConfiguredForGhul();
 
 module.exports = {
   /**
