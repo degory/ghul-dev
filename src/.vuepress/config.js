51d597f53b9b32e2921ee2f4fdfd8df67712d872
degory <degs@giantblob.com>
2024-01-22 15:44:07 +0000

    Add a favicon

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -24,6 +24,7 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['link', { rel: 'stylesheet', href: '/prism-vsc-dark-plus.css' }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }],    
   ],
 
   /**
