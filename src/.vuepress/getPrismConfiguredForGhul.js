6cd1a967fe952042b3e1857bc0052eecfcbe8060
degory <degs@giantblob.com>
2024-03-19 22:04:24 +0000

    Improve code highlighting for string interpolations

--- src/.vuepress/getPrismConfiguredForGhul.js
+++ src/.vuepress/getPrismConfiguredForGhul.js
@@ -4,15 +4,37 @@ function getPrismConfiguredForGhul() {
   if (!Prism.languages.ghul) {
     console.log('set-up Prism.languages.ghul...');
 
+    const interpolationPattern = {
+      pattern: /\{[^{}]*\}/,
+      inside: {
+        'interpolation-punctuation': {
+          pattern: /^\{|\}$/,
+          alias: 'punctuation'
+        },
+        'control': /\b(assert|if|then|elif|else|fi|for|in|while|do|od|continue|break|case|when|default|esac|throw|try|catch|finally|yrt|return)\b/,
+        'keyword': /\b(new|cast|isa|namespace|class|trait|struct|enum|use|is|si|let|ptr|ref|static|public|protected|private|typeof|rec|->|=>)\b/,
+        'boolean': /\b(?:true|false|null)\b/,
+        'class-name': /\b([A-Z][A-Za-z0-9_`]*|void|bool|byte|ubyte|short|ushort|int|uint|long|ulong|word|uword|char|object|string|single|double)\b/,
+        'function': /\b([a-zA-Z_][a-zA-Z0-9_]*)(?=\s*\()/,
+        'number': /\b-?\d*\.?[\d]+\b/,
+        'operator': /(!|\$|%|\^|&|\*|-|\+|=|\||@|~|#|\\|<|>|\.|\?|\/|\*)+/,
+        'punctuation': /[;[\](),.]/,
+        'variable': /\b([`a-zA-Z_][a-zA-Z0-9_]*)\b/,
+        }
+    };    
+
     Prism.languages.ghul = {
       'comment': {
         pattern: /(^|[^\\])(?:\/\*[\s\S]*?\*\/|(?:\/\/).*)/,
         lookbehind: true
       },
       'string': {
-        pattern: /(^|[^@\\])("|')(?:\\?[\s\S])*?\2/,
+        pattern: /(^|[^@\\])"(?:\\[\s\S]|[^\\"]|\{(?:\\[\s\S]|[^\\}])*\})*"/,
         greedy: true,
-        lookbehind: true
+        lookbehind: true,
+        inside: {
+          'interpolation': interpolationPattern
+        }
       },
       'control': /\b(assert|if|then|elif|else|fi|for|in|while|do|od|continue|break|case|when|default|esac|throw|try|catch|finally|yrt|return)\b/,
       'keyword': /\b(new|cast|isa|namespace|class|trait|struct|enum|use|is|si|let|ptr|ref|static|public|protected|private|typeof|rec|->|=>)\b/,
